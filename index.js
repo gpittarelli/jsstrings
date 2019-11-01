75678f1f5f395edd8d82af8d7799caeb309dbb20
George Pittarelli <g@gjp.cc>
2019-11-01 13:24:37 -0700

    Initial implementation

---
+++ index.js
@@ -0,0 +1,54 @@
+const babylon = require('@babel/parser');
+const traverse = require('@babel/traverse').default;
+const t = require('@babel/types');
+
+function parseVisitor(code, visitor, onComplete) {
+  // accept either pre-parsed ast or string of code
+  if (typeof code === 'string') {
+    code = babylon.parse(code, {
+      sourceType: 'module',
+      plugins: [
+        'asyncGenerators',
+        'classConstructorCall',
+        'classProperties',
+        'decorators-legacy',
+        'doExpressions',
+        'exportDefaultFrom',
+        'exportExtensions',
+        'flow',
+        'functionSent',
+        'functionBind',
+        'jsx',
+        'objectRestSpread',
+        'optionalChaining',
+        'dynamicImport'
+      ]
+    });
+  }
+
+  traverse(code, {
+    enter: function(path) {
+      var node = path.node;
+
+      if (path.isStringLiteral()) {
+        visitor(node.value);
+      } else if (path.isTemplateLiteral()) {
+        for (const q of node.quasis) {
+          visitor(q.value.raw);
+        }
+      }
+    }
+  });
+  (' yooooo ');
+}
+
+function parse(code) {
+  const out = [];
+  parseVisitor(code, str => out.push(str));
+  return out;
+}
+
+module.exports = {
+  parse,
+  parseVisitor
+};
